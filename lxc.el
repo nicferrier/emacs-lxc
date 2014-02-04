728f06d63e4b364f2c8bb036f441b7ebc123a2ec
Nic Ferrier <nferrier@ferrier.me.uk>
2014-02-04 16:01:54 +0000

    make it packageable

--- lxc.el
+++ lxc.el
@@ -4,6 +4,7 @@
 
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Keywords: processes
+;; Version: 0.0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
